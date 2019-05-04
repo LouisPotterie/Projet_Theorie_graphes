2de25aadd27485faeede49c62e2c91826f24e844
Louis <louis.potterie@efrei.net>
2019-05-04 19:15:37 +0200

    avec code de pa

--- src/com/company/L3_D4_Main.java
+++ src/com/company/L3_D4_Main.java
@@ -371,14 +371,14 @@ public class L3_D4_Main
         //initialisation d'un graph sous la forme d'un tableau de transition
 
 
-        int nombreSommets = graphe.getNombreSommets();
-        int tableau_transitions[][] = new int[graphe.getNombreTransitions()][3];
-        int transition = 0;
+        int nombreSommets= graphe.getNombreSommets();
+        int tableau_transitions[][]= new int[graphe.getNombreTransitions()][3];
+        int transition=0;
         for (Noeud noeud : graphe.getNoeuds())
         {
             for (Map.Entry<Noeud, Integer> entry : noeud.getSuccesseurs().entrySet())
             {
-                tableau_transitions[transition] = new int[]{noeud.getSommet(), entry.getKey().getSommet(), entry.getValue()};
+                tableau_transitions[transition]= new int[]{noeud.getSommet(), entry.getKey().getSommet(), entry.getValue()};
                 transition++;
             }
         }
@@ -401,7 +401,7 @@ public class L3_D4_Main
             (en ligne) de k = 0 à k = nombreSommets - 1
             (en colonne) tous les sommets, donc nombreSommets
         */
-        int tableau_de_k[][] = new int[nombreSommets][nombreSommets];
+        int tableau_de_k[][] = new int[nombreSommets+1][nombreSommets];
 
         /*
             Tableau des predecesseurs
@@ -409,16 +409,14 @@ public class L3_D4_Main
             meme dimensions que le tableau des k
             il permet de recuperer les prédecesseurs permettant de dresser le chemin le plus cours
          */
-        int tableau_de_predecesseur[][] = new int[nombreSommets][nombreSommets];
+        int tableau_de_predecesseur[][] = new int[nombreSommets+1][nombreSommets];
 
         // Le MAX_SIZE permet de remplacer le signe infini dans le tableau des k
-        int MAX_SIZE = Integer.MAX_VALUE;
+        int MAX_SIZE = 999;
 
         // On initialise le tableau des k avec la variable MAX_SIZE (inifini)
-        for (int i = 0; i < nombreSommets; i++)
-        {
-            for (int n = 0; n < nombreSommets; n++)
-            {
+        for (int i = 0; i < nombreSommets+1; i++){
+            for (int n =0; n < nombreSommets; n++){
                 tableau_de_k[i][n] = MAX_SIZE;
             }
         }
@@ -428,24 +426,11 @@ public class L3_D4_Main
         sommet_depart = 0;
 
         //initialisation de la colonne du sommet de départ
-        for (int j = 0; j < nombreSommets; j++)
+        for(int j = 0; j < nombreSommets+1; j++)
         {
-            tableau_de_k[j][sommet_depart] = 0;
-            tableau_de_predecesseur[j][sommet_depart] = sommet_depart;
+            tableau_de_k[j][sommet_depart]=0;
+            tableau_de_predecesseur[j][sommet_depart]=sommet_depart;
         }
-
-        //Savoir si le graph est absorbant ou pas ?
-        int absorbant_ou_pas;
-        int plus_petite_transition = 0;
-
-        for (int recherche_absorbant_ou_pas = 0; recherche_absorbant_ou_pas < nombreTransitions; recherche_absorbant_ou_pas++)
-        {
-            plus_petite_transition = Math.min(plus_petite_transition, tableau_transitions[recherche_absorbant_ou_pas][2]);
-        }
-        if (plus_petite_transition < 0)
-            absorbant_ou_pas = 0;
-        else
-            absorbant_ou_pas = 1;
 
         /*
             initialisation des différentes variables
@@ -456,76 +441,43 @@ public class L3_D4_Main
         int succ_v2 = 0;
         int value = 0;
         int v = 0;
-        int suivant = 1;
         int compteur = 0;
 
-        //Si le graph a un circuit absorbant
-        if (absorbant_ou_pas == 0)
-        {
-            for (int k = 1; k < nombreSommets; k++)
-            {
-                for (int o = 0; o < nombreTransitions; o++)
-                {
+        for (int k = 1; k < nombreSommets+1; k++) {
+            for (int o = 0; o < nombreTransitions; o++){
 
-                    prec = tableau_transitions[o][0]; //on récup le prec grace au graph
-                    succ = tableau_transitions[o][1];
-                    value = tableau_transitions[o][2];
+                prec = tableau_transitions[o][0]; //on récup le prec grace au graph
+                succ = tableau_transitions[o][1];
+                value = tableau_transitions[o][2];
 
-                    prec_v2 = tableau_de_k[k - 1][prec]; //on recup le poids pour le prec dans le tableau k
-                    succ_v2 = tableau_de_k[k - 1][succ];
+                prec_v2 = tableau_de_k[k-1][prec]; //on recup le poids pour le prec dans le tableau k
+                succ_v2 = tableau_de_k[k-1][succ];
 
-                    v = Math.min(succ_v2, (prec_v2 + value)); //on cherche le min
+                v = Math.min(succ_v2, (prec_v2 + value)); //on cherche le min
 
-                    if (v < tableau_de_k[k][succ])
-                    {
-                        tableau_de_k[k][succ] = v; // on modifie la valeur dans le tableau k pour avoir le plus petit chemin
-                        if (v != tableau_de_k[k - 1][succ])
-                        {
-                            tableau_de_predecesseur[k][succ] = prec;
-                        }
+                if ( v < tableau_de_k[k][succ]){
+                    tableau_de_k[k][succ] = v; // on modifie la valeur dans le tableau k pour avoir le plus petit chemin
+                    if (v != tableau_de_k[k-1][succ]) {
+                        tableau_de_predecesseur[k][succ] = prec;
                     }
                 }
             }
-        } else
-        { //si le graph n'a pas de circuit absorbant
-            int k = 1;
-            while (suivant == 1)
-            {
-                for (int o = 0; o < nombreTransitions; o++)
-                {
+        }
 
-                    prec = tableau_transitions[o][0]; //on récup le prec grace au graph
-                    succ = tableau_transitions[o][1];
-                    value = tableau_transitions[o][2];
+        int absorbant = 0;
 
-                    prec_v2 = tableau_de_k[k - 1][prec]; //on recup le poids pour le prec dans le tableau k
-                    succ_v2 = tableau_de_k[k - 1][succ];
-
-                    v = Math.min(succ_v2, (prec_v2 + value)); //on cherche le min
-
-                    if (v < tableau_de_k[k][succ])
-                    {
-                        tableau_de_k[k][succ] = v; // on modifie la valeur dans le tableau k pour avoir le plus petit chemin
-                        if (v != tableau_de_k[k - 1][succ])
-                        {
-                            tableau_de_predecesseur[k][succ] = prec;
-                        }
-                    }
-                }
-
-                for (int m = 0; m < nombreSommets; m++)
-                {
-                    if (tableau_de_k[k - 1][m] == tableau_de_k[k][m])
-                    {
-                        compteur++;
-                    }
-                }
-                if (compteur == nombreSommets)
-                    suivant = 0;
-
-                compteur = 0;
-                k++;
+        for (int m = 0; m < nombreSommets; m++){
+            if (tableau_de_k[nombreSommets-1][m] == tableau_de_k[nombreSommets][m]){
+                compteur ++;
             }
+        }
+        if (compteur == nombreSommets){
+            System.out.println("Ce graphe contient un circuit absorbant");
+            absorbant = 1;
+        }
+        else{
+            System.out.println("Ce graphe ne contient pas de circuit absorbant");
+
         }
 
         /*
@@ -534,27 +486,48 @@ public class L3_D4_Main
         int a = 1;
         int b;
 
-        while (a < nombreSommets)
+        while (a < nombreSommets+1)
         {
             b = 0;
-            while (b < nombreSommets)
+
+            while(b < nombreSommets)
             {
-                if (tableau_de_k[a][b] > 900)
-                {
+                if (tableau_de_k[a][b] > 900) {
                     System.out.printf("%4s    |", "*");
-                } else
-                {
-                    if (tableau_de_predecesseur[a][b] == 0)
-                    {
-                        tableau_de_predecesseur[a][b] = tableau_de_predecesseur[a - 1][b];
+                }
+                else {
+                    if (tableau_de_predecesseur[a][b] == 0){
+                        tableau_de_predecesseur[a][b] = tableau_de_predecesseur[a-1][b];
                     }
-                    System.out.printf("%3d (%d) |", tableau_de_k[a][b], tableau_de_predecesseur[a][b]);
+                    System.out.printf("%3d (%d) |",tableau_de_k[a][b], tableau_de_predecesseur[a][b]);
                 }
                 b++;
             }
             System.out.println("");
             a++;
         }
+
+        if (absorbant == 0) {
+            int w = MAX_SIZE;
+            int p = MAX_SIZE;
+            int longueur = 0;
+
+            for (int ii = 0; ii < nombreSommets; ii++){
+                p = ii;
+                System.out.println("Le chemin le plus court du sommet "+ii+" au sommet initial "+sommet_depart+" est : ");
+                w = MAX_SIZE;
+                while (w != sommet_depart){
+                    p = tableau_de_predecesseur[nombreSommets-1][p];
+                    longueur += tableau_de_k[nombreSommets-1][p];
+                    System.out.print(" " +p);
+                    w = p;
+                }
+                System.out.println(" et de longueur : "+longueur);
+                System.out.println(" ");
+                longueur = 0;
+            }
+        }
+
 
     }
     static void enregistrementTrace(Graphe graphe, int numGraphe, int sommetDepart, Integer[][] dijkstra)throws FileNotFoundException
